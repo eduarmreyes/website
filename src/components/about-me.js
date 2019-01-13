ba37dd078013fbb3f4e8d5b83884f02cb7f208d0
eduarmreyes <eduar.mreyes@gmail.com>
2019-01-12 21:44:09 -0600

    update information about me

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -18,10 +18,13 @@ const AboutMe = ({ author, from, cta_text }) => (
           Hi, I'm Eduardo Mejía, Front End Developer from El Salvador, living in the US.
         </h4>
         <p className="mb-4 leading-normal">
-          I am from <a href="https://elsalvador.travel/en/" target="_blank" rel="noopener noreferrer">El Salvador</a>. A nice country with a lot of good things in a tiny amount of earth.
+          I am from <a href="https://elsalvador.travel/en/" target="_blank" rel="noopener noreferrer">El Salvador</a>. A nice country with a lot of good things in a tiny amount of earth. It's so tiny that you're able to be at the beach in the morning and have a great old town tour at the afternoon, you could even go to a nice mall at night and be back to your place all in one day.
         </p>
         <p className="mb-4 leading-normal">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, porro voluptas illo aspernatur fugit laboriosam rerum impedit? Neque sequi nemo deserunt temporibus harum quam, nobis cum explicabo omnis vel ipsum!
+          I decided to be a Software Developer because I like the idea to create nice things to make life easier to others, I also was impacted when I realized I could be really good at this just by having a laptop and internet, the fact that a boy from El Salvador can compite with people that was upbrought in a country with different opportunities gave me hope my future was as bright as my imagination.
+        </p>
+        <p className="mb-4 leading-normal">
+          Fun fact about me, I am a former shortstop baseball player, I even played representing my country for a couple of years, wonderful, amazing, fill with beautiful memories.
         </p>
         <a href="mailto:eduar.mreyes@gmail.com" className="no-underline"><span role="img" aria-label="email emoji">📧</span> Send me an email</a>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,6 @@ import AboutMe from '../components/about-me'
 const IndexPage = () => (
   <Layout>
     <AboutMe />
-    <h1 className="mb-4">Hi everyone</h1>
-    <p className="leading-loose">This is going to be my landing, changes coming up.</p>
-    <p className="leading-loose">Built upon great technologies</p>
-    <ul>
-      <li>
-        Gatsby
-      </li>
-      <li>
-        ReactJS
-      </li>
-      <li>
-        NodeJS
-      </li>
-    </ul>
   </Layout>
 )
 
