6515e9d444c9114f5de8f2e824edba94b37922a5
Eduardo <eduar.mreyes@gmail.com>
2019-03-04 08:11:55 -0500

    add front end work process boxes

--- src/components/working-process.js
+++ src/components/working-process.js
@@ -7,9 +7,43 @@ const WorkingProcess = ({ author, from, cta_text }) => (
       <h1 className="text-center">
         My Working Process
       </h1>
-      <h3 className="text-grey-darker text-center mt-4">
+      <h3 className="text-grey-darker text-center mt-6 mb-6">
         As a Front End developer, the way I craft out great websites is as follows
       </h3>
+      <div class="flex">
+        <div class="flex-1 text-grey-darker text-center bg-teal-dark px-6 py-6 m-2">
+          <span role="img" aria-label="artist palette emoji">
+            🎨
+          </span>
+          <p className="mt-4 text-teal-lightest">
+            Design
+          </p>
+        </div>
+        <div class="flex-1 text-grey-darker text-center bg-blue-dark px-6 py-6 m-2">
+          <span role="img" aria-label="laptop computer emoji">
+            💻
+          </span>
+          <p className="mt-4 text-blue-lightest">
+            Development
+          </p>
+        </div>
+        <div class="flex-1 text-grey-darker text-center bg-indigo-dark px-6 py-6 m-2">
+          <span role="img" aria-label="computer emoji">
+            📊
+          </span>
+          <p className="mt-4 text-indigo-lightest">
+            Testing
+          </p>
+        </div>
+        <div class="flex-1 text-grey-darker text-center bg-pink-dark px-6 py-6 m-2">
+          <span role="img" aria-label="computer emoji">
+            🚀
+          </span>
+          <p className="mt-4 text-pink-lightest">
+            Launch
+          </p>
+        </div>
+      </div>
     </div>
   </section>
 )
