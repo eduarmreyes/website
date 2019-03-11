2e85daabab38052dbbc2d82685fcb158c80df961
Eduardo <eduar.mreyes@gmail.com>
2019-03-11 05:27:55 -0400

    add leading normal class for better readability on paragraphs

--- src/components/experience.js
+++ src/components/experience.js
@@ -15,7 +15,7 @@ const Experience = () => (
           <h4 className="mb-4">
             FEB '16 - SEP '17 & SEP'18 - <i>CURRENT</i>
           </h4>
-          <p className="mb-4">
+          <p className="mb-4 leading-normal">
             Part of the team that created <a className="text-white" href="https://usabili.me">https://usabili.me</a>. In charge of most of the Front End development of the product. Development was based on agile methodology.
           </p>
           <hr className="border border-solid border-blue-lightest mb-4"></hr>
@@ -25,7 +25,7 @@ const Experience = () => (
           <h4 className="mb-4">
             Sep '17 - Sep '18
           </h4>
-          <p className="mb-4">
+          <p className="mb-4 leading-normal">
             In charge of the Front End development of TechLatam's web-based projects such as websites and dashboards. Responsible for <strong>top-notch web development</strong> based on beautiful designs.
           </p>
         </div>
@@ -39,7 +39,7 @@ const Experience = () => (
           <h4 className="mb-4">
             '13 - '18
           </h4>
-          <p className="mb-4">
+          <p className="mb-4 leading-normal">
             The most important part of my education is understand that what I learned in the university will be obsolete pretty fast and that we need to continue our education everyday.
           </p>
           <hr className="border border-solid border-blue-lightest mb-4"></hr>
@@ -49,7 +49,7 @@ const Experience = () => (
           <h4 className="mb-4">
             '10 - '11
           </h4>
-          <p className="mb-4">
+          <p className="mb-4 leading-normal">
             Started my web development career, it was crazy, we designed our websites using <code>tables</code> crazy days.
           </p>
         </div>
