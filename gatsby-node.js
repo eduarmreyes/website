2a94c7d54604910fad1f080f4fbc5a3299df07c1
Eduardo <eduar.mreyes@gmail.com>
2019-03-15 07:42:59 -0400

    remove extra lines in gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,6 @@
 const path = require('path');
-// import { resolve as _resolve } from 'path';
 
 // Create pages from markdown files
-
-
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
   return new Promise((resolve, reject) => {
