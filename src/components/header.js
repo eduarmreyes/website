94d3655f8c990f23bc8aded8381787b923b9e383
Eduardo <eduar.mreyes@gmail.com>
2019-03-18 07:31:41 -0400

    show bg el salvador colorized and better reading style on work xp and education cards

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 
 const Header = ({ author }) => (
-  <header className="min-h-screen text-center bg-purple-darker border-t-6 border-blue-light flex items-center justify-center">
+  <header className="min-h-half-screen text-center bg-purple-darker border-t-6 border-blue-light flex items-center justify-center">
     <section className="container-hero padding-t-4 padding-b-4">
       <h1 className="text-8xl pt-3 pb-3 border-t-3 border-b-3 border-blue-light">
         <Link to="/" className="text-purple-lightest no-underline">
